23ec1c8eb4632267fa128afcbda3165c00e74852
Felipe de Siqueira Zanella <freeman.zanella@gmail.com>
2018-06-04 22:20:09 -0300

    DAO de pessoa

--- app/dao/pessoa.js
+++ app/dao/pessoa.js
@@ -4,7 +4,13 @@ module.exports = {
 
     insert: (data, onSuccess, onError) => {
 
-        // TODO - fazer query
+        const query = 'INSERT INTO pessoa (ds_nome, ds_cpf, dt_nascimento, ds_telefone, ds_celular, ds_email)values (\'#\',\'#\',#,\'#\',\'#\',\'#\');'
+            .replace('#', data.dsNome)
+            .replace('#', data.dsCpf)
+            .replace('#', data.dtNascimento)
+            .replace('#', data.dsTelefone)
+            .replace('#', data.dsCelular)
+            .replace('#', data.dsEmail);
 
         dao.exec(query, onSuccess, onError);
 
@@ -12,7 +18,13 @@ module.exports = {
 
     update: (data, onSuccess, onError) => {
 
-        // TODO - fazer query
+        const query = 'UPDATE pessoa SET ds_nome = \'#\', ds_cpf = \'#\', dt_nascimento = #, ds_telefone = \'#\', ds_celular = \'#\', ds_email = \'#\' WHERE id_pessoa = #;'
+            .replace('#', data.dsNome)
+            .replace('#', data.dsCpf)
+            .replace('#', data.dtNascimento)
+            .replace('#', data.dsTelefone)
+            .replace('#', data.dsCelular)
+            .replace('#', data.dsEmail);
 
         dao.exec(query, onSuccess, onError);
 
@@ -20,7 +32,15 @@ module.exports = {
 
     findAll: (onSuccess, onError) => {
 
-        // TODO - fazer query
+        const query = `SELECT
+        id_pessoa as idPessoa,
+        ds_nome as dsNome,
+        ds_cpf as dsCpf,
+        dt_nascimento as dtNascimento,
+        ds_telefone as dsTelefone,
+        ds_celular as dsCelular,
+        ds_email as dsEmail 
+        FROM pessoa;`
 
         dao.exec(query, onSuccess, onError);
 
@@ -28,7 +48,15 @@ module.exports = {
 
     findById: (id, onSuccess, onError) => {
 
-        // TODO - fazer query
+        const query = `SELECT
+        id_pessoa as idPessoa,
+        ds_nome as dsNome,
+        ds_cpf as dsCpf,
+        dt_nascimento as dtNascimento,
+        ds_telefone as dsTelefone,
+        ds_celular as dsCelular,
+        ds_email as dsEmail 
+        FROM pessoa WHERE id_pessoa = # LIMIT 1;`.replace('#', id);
 
         dao.exec(query, onSuccess, onError);
 
@@ -36,7 +64,7 @@ module.exports = {
 
     remove: (id, onSuccess, onError) => {
 
-        // TODO - fazer query
+        const query = 'DELETE FROM pessoa WHERE id_pessoa = #;'.replace('#', id);
 
         dao.exec(query, onSuccess, onError);
 
